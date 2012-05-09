43a3007671080011f7ff990c4e3f07cda7c0d4b4
Robin <gl.deng@gmail.com>
2012-05-09 18:40:40 +0800

    file upload try

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 
 var express = require('express')
   , markdown = require('markdown')
+  , fs = require('fs')
   , routes = require('./routes');
 
 var app = module.exports = express.createServer();
@@ -15,7 +16,7 @@ app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.set('view options', {'layout': false, 'pretty': false}); //不需要layout模版
-  app.use(express.bodyParser());
+  app.use(express.bodyParser({uploadDir:'/public/uploads'}));
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
@@ -49,6 +50,10 @@ app.get('/post/:id', function(req, res){
   routes.single(req, res);
 });
 
+app.post('/post/addComment', function(req, res){
+  routes.addComment(req, res);
+});
+
 app.get('/tag/:tag', function(req, res){
   routes.tag(req, res);
 });
@@ -57,12 +62,26 @@ app.get('/tags', function(req, res){
   routes.tags(req, res);
 });
 
-app.post('/post/addComment', function(req, res){
-  routes.addComment(req, res);
-});
-
 app.get('/admin', function(req, res){
   routes.admin(req, res);
+});
+
+app.post('/admin/fileupload', function(req, res){
+  console.log(req.body);
+  console.log(req.files);
+
+  console.log(req.files.thumbnail.path);
+
+  var tmp_path = req.files.thumbnail.path;
+  var target_path = '/public/uploads/' + req.files.thumbnail.name; 
+
+  fs.rename(tmp_path, target_path, function(err){
+    if (err) throw err;
+    fs.unlink(tmp_path, function (err) {
+      if (err) throw err;
+      res.send({'r': 'success'});
+    })
+  });
 });
 
 app.get('/admin/post/list', function(req, res){
--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -3,4 +3,14 @@ $(function(){
 	$("#post-publist").click(function(){
 		$("#new-post").submit();
 	})
+
+  //uploader
+  $("#fileupload").iframePostForm({
+    post: function(){},
+    complete: function (o) {
+        var imgData = $.parseJSON($.browser.msie ? o : $(o).text());
+        //$("#file_list").append('<img src="' + imgData['src'].replace('public', '') + '" />');
+        $("#file_list").append('<img src="http://img1.douban.com/lpic/s9014462.jpg" width="130" />');
+    }
+  })
 })
